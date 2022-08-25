b4f9a3105a836236c2dba8915de66556864c3a16
Courtney Lippman <courtney.lippman@colorado.edu>
2022-08-25 07:36:13 -0600

    Create first try for buildMainCharacter.

--- src/book.js
+++ src/book.js
@@ -2,9 +2,23 @@ function createTitle(title) {
   return `The ${title}`;
 }
 
+function buildMainCharacter() {
+ var ghoulCharacter = {
+   name:'Vassya',
+  age:16,
+ pronouns:'she/her'
+};
+var bakerCharacter = {
+  name:'Hannah',
+ age:30,
+pronouns:'she/her'
+};
+}
+
+
 module.exports = {
   createTitle,
-  // buildMainCharacter,
+  buildMainCharacter,
   // saveReview,
   // calculatePageCount,
   // writeBook,
