72eada0cd6f1acb5da6818f1ee0f291541d07f08
Courtney Lippman <courtney.lippman@colorado.edu>
2022-08-24 19:13:31 -0600

    Add answers to variables section.

--- src/warm-up.js
+++ src/warm-up.js
@@ -10,29 +10,33 @@
 // Initialize two new variables, "title", and "pages", and assign values to them with
 // a book's title and page count.
 
-// var title = "Harry Potter";
-// var ...
+var title = "The Lord Of The Rings";
+var pages = 1178;
 
 
 
 // Reassign (not initialize) your "pages" variable from above. An editor came around and
 // condensed the book, use a built in math operator to make the page count a quarter
-// of what it currently is 
+// of what it currently is
 
+pages = 1178 / 4;
 
 
 // Initialize a new variable "movie", but leave it undefined
+
+var movie;
 
 
 
 // Create a variable named "sequel", and assign it to a be a sequel of your
 // book (made up or not!). Your sequel must make use of your "title" variable from above
 
-
+var squel = `The Fellowship of the Ring is the sequel to ${title}.`;
 
 // Initialize a new variable "isFavorite", and assign it to a boolean value indicating
 // if the book you chose is your favorite book or not
 
+var isFavorite = false;
 
 
 
@@ -41,13 +45,19 @@
 // variable should be true, otherwise it should be false. You MUST use a
 // comparison operator.
 
+var isShortStory
 
-
+if (pages < 25) {
+  isShortStory = true;
+} else {
+  isShortStory = false;
+}
+console.log(isShortStory)
 //------------------------- Conditionals -------------------------//
 
 
 // Express the following in code: If the variable "pages" is less than 1000 log
-// the statement 'I could read that in a day!', otherwise, log the statement 
+// the statement 'I could read that in a day!', otherwise, log the statement
 // 'Ok, maybe I could read that in a day and a half'
 
 
