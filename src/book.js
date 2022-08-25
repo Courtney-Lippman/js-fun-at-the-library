897204957d57d1ff66b267829989a2540f06e378
Courtney Lippman <courtney.lippman@colorado.edu>
2022-08-25 09:22:19 -0600

    Add correct solution for object test for buildMainCharacter.

--- src/book.js
+++ src/book.js
@@ -2,17 +2,14 @@ function createTitle(title) {
   return `The ${title}`;
 }
 
-function buildMainCharacter() {
- var ghoulCharacter = {
-   name:'Vassya',
-  age:16,
- pronouns:'she/her'
-};
-var bakerCharacter = {
-  name:'Hannah',
- age:30,
-pronouns:'she/her'
-};
+function buildMainCharacter(name, age, pronouns) {
+var mainCharacter =  {
+    name: name,
+  age: age,
+  pronouns: pronouns
+}
+return mainCharacter;
+
 }
 
 
