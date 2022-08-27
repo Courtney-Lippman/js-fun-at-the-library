de5b5d6f64f6e00ac0937997eaf49fb2c39205ca
Courtney Lippman <courtney.lippman@colorado.edu>
2022-08-27 08:31:22 -0600

    Add all passing functions.

--- src/shelf.js
+++ src/shelf.js
@@ -1,7 +1,43 @@
+function shelfBook(book, shelf) {
+  if (shelf.length < 3) {
+      shelf.unshift(book);
+  };
+};
+
+function unshelfBook(title, shelf) {
+  for (var i = 0; i < shelf.length; i++) {
+    if (title === shelf[i].title) {
+      shelf.splice(i,1);
+    };
+  };
+};
+
+function listTitles(shelf) {
+  var allTitles = '';
+  for (let i = 0; i < shelf.length; i++) {
+    var title = shelf[i].title;
+    if (i === 0) {
+         allTitles += title;
+    } else
+         allTitles += ", " + title;
+    };
+return allTitles;
+};
+
+function searchShelf(shelf, title) {
+  for (var i = 0; i < shelf.length; i++) {
+    if (shelf[i].title === title) {
+     var onShelf = true;
+    } else {
+     onShelf = false;
+    };
+  };
+return onShelf;
+}
 
 module.exports = {
-  // shelfBook,
-  // unshelfBook,
-  // listTitles,
-  // searchShelf
+  shelfBook,
+  unshelfBook,
+  listTitles,
+  searchShelf
 };
